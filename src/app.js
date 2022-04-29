ac338da8c7b09d3e5f42d4ac3617b8b3c8126248
Nivaldo Farias <badivia.f@gmail.com>
2022-04-29 13:52:11 -0300

    Added endpoints for participants and messages

--- src/app.js
+++ src/app.js
@@ -9,19 +9,24 @@ dotenv.config();
 
 let database = null;
 const app = express().use(json()).use(cors());
-const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
+const SERVER_INFO = chalk.bold.yellow("[Server]");
+const API_INFO = chalk.bold.blue("[API]");
+const DB_INFO = chalk.bold.green("[Database]");
+const ERROR = chalk.bold.red("[ERROR]");
 const regex = /^(message|private_message)$/;
 const participantSchema = Joi.object({
-  name: Joi.string().alphanum().min(1).max(25).required(),
+  name: Joi.string().min(1).max(25).required(),
 });
 const messageSchema = Joi.object({
-  to: Joi.string().alphanum().min(1).max(25).required(),
+  to: Joi.string().min(1).max(25).required(),
   text: Joi.string().min(1).required(),
   type: Joi.string().regex(regex).required(),
 });
 const PARTICIPANTS_PATH = "/participants";
 const MESSAGES_PATH = "/messages";
+const STATUS_PATH = "/status";
+const PORT = process.env.PORT || 5000;
 const mongoClient = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -30,8 +35,10 @@ const mongoClient = new MongoClient(uri, {
 mongoClient.connect(() => {
   database = mongoClient.db("chat_uol");
   console.log(
-    chalk.bold.green(
-      `Connected to database "${database.databaseName}" at ${mongoClient.s.url}`
+    chalk.blue(
+      `${API_INFO} Connected to database ${chalk.underline.blue(
+        database.databaseName
+      )}`
     )
   );
 });
@@ -44,44 +51,191 @@ app.get(PARTICIPANTS_PATH, async (req, res) => {
       .toArray();
     res.send(participants);
   } catch (err) {
-    console.log(chalk.bold.red(err));
+    console.log(chalk.red(`${ERROR} ${err}`));
     res.status(500).send(err);
   }
 });
 
 app.post(PARTICIPANTS_PATH, async (req, res) => {
-  const { name: participant } = req.body;
+  const participant = req.body;
+
   try {
-    const validate = Joi.validate(participant, participantSchema);
-    validate.error
-      ? res.status(422).send(validate.error.details[0].message)
-      : null;
+    const validate = participantSchema.validate(participant, {
+      abortEarly: true,
+    });
+    if (validate.error) {
+      console.log(chalk.red(`${ERROR} ${validate.error.details[0].message}`));
+      res.status(422).send(validate.error.details[0]);
+      return;
+    }
 
     const participantExists = await database
       .collection("participants")
-      .findOne({ name: participant });
-    participantExists
-      ? res.status(409).send("Este participante já existe")
-      : null;
+      .findOne({ name: participant.name });
+    if (participantExists) {
+      console.log(
+        chalk.red(
+          `${ERROR} Participant ${chalk.underline(
+            participant.name
+          )} already exists`
+        )
+      );
+      res.status(409).send("Este participante já existe");
+      return;
+    }
 
-    await database.collection("participants").insertOne({
-      name: participant,
-      lastStatus: Date.now(),
-    });
-    await database.collection("messages").insertOne({
-      from: participant,
-      to: "Todos",
-      text: "entra na sala...",
-      type: "status",
-      time: new Date().toLocaleTimeString(),
-    });
-    res.sendStatus(201);
+    try {
+      await database.collection("participants").insertOne({
+        name: participant.name,
+        lastStatus: Date.now(),
+      });
+      await database.collection("messages").insertOne({
+        from: participant.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: new Date().toLocaleTimeString(),
+      });
+      console.log(chalk.green(`${DB_INFO} ${participant.name} inserted`));
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(chalk.red(`${ERROR} ${err}`));
+      res.status(500).send(err);
+    }
   } catch (err) {
     console.log(chalk.bold.red(err));
     res.status(500).send(err);
   }
 });
 
+app.get(MESSAGES_PATH, async (req, res) => {
+  const user = req.header("user");
+  if (user === undefined) {
+    console.log(chalk.red(`${ERROR} No user header`));
+    res.status(400).send({ error: "No user header" });
+    return;
+  }
+  try {
+    const messages = await database.collection("messages").find().toArray();
+    const limit = parseInt(req.query.limit) || messages.length;
+    const output = messages
+      .reverse()
+      .slice(0, limit)
+      .filter((message) => {
+        return (
+          message.to === user || message.to === "Todos" || message.from === user
+        );
+      });
+    res.send(output);
+  } catch (err) {
+    console.log(chalk.red(`${ERROR} ${err}`));
+    res.status(500).send(err);
+  }
+});
+
+app.post(MESSAGES_PATH, async (req, res) => {
+  const message = req.body;
+  try {
+    const validate = messageSchema.validate(message, {
+      abortEarly: true,
+    });
+    if (validate.error) {
+      console.log(chalk.red(`${ERROR} ${validate.error.details[0].message}`));
+      res.status(422).send(validate.error.details[0]);
+      return;
+    }
+
+    const destinatary = await database
+      .collection("participants")
+      .findOne({ name: message.to });
+    if (!destinatary) {
+      console.log(
+        chalk.red(
+          `${ERROR} Participant ${chalk.underline(message.to)} does not exist`
+        )
+      );
+      res.status(404).send({ error: "Este participante não existe" });
+      return;
+    }
+
+    const user = req.header("user");
+    if (user === undefined) {
+      console.log(chalk.red(`${ERROR} No user header`));
+      res.status(400).send({ error: "No user header" });
+      return;
+    }
+
+    const remetent = await database
+      .collection("participants")
+      .findOne({ name: user });
+    if (!remetent) {
+      console.log(
+        chalk.red(
+          `${ERROR} Participant ${chalk.underline(user)} does not exist`
+        )
+      );
+      res.sendStatus(422);
+      return;
+    }
+
+    try {
+      await database.collection("messages").insertOne({
+        from: user,
+        to: message.to,
+        text: message.text,
+        type: message.type,
+        time: new Date().toLocaleTimeString(),
+      });
+      console.log(chalk.green(`${DB_INFO} message from ${user} sent`));
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(chalk.red(`${ERROR} ${err}`));
+      res.status(500).send(err);
+    }
+  } catch (err) {
+    console.log(chalk.red(`${ERROR} ${err}`));
+    res.status(500).send(err);
+  }
+});
+
+app.post(STATUS_PATH, async (req, res) => {
+  const user = req.header("user");
+  if (user === undefined) {
+    console.log(chalk.red(`${ERROR} No user header`));
+    res.status(400).send({ error: "No user header" });
+    return;
+  }
+
+  try {
+    const participant = await database
+      .collection("participants")
+      .findOne({ name: user });
+    if (!participant) {
+      console.log(
+        chalk.red(
+          `${ERROR} Participant ${chalk.underline(user)} does not exist`
+        )
+      );
+      res.sendStatus(422);
+      return;
+    }
+
+    try {
+      await database
+        .collection("participants")
+        .updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+      console.log(chalk.green(`${DB_INFO} ${user} status updated`));
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(chalk.red(`${ERROR} ${err}`));
+      res.status(500).send(err);
+    }
+  } catch (err) {
+    console.log(chalk.red(`${ERROR} ${err}`));
+    res.status(500).send(err);
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(chalk.bold.yellow(`Server running on port ${PORT}`));
+  console.log(chalk.bold.yellow(`${SERVER_INFO} running on port ${PORT}`));
 });
